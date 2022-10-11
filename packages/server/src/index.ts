c9b6bf25ba6effcfc7596d339bd760bdbb0ce347
Teemu Koivisto <tkoivisto456@gmail.com>
2022-10-12 00:29:19 +0300

    ci: test example fly express app

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,16 +1,27 @@
-import { enableDebug } from '@example/game'
-import { app } from './app'
-import { config, log } from './common'
-import { Server } from './Server'
+import express from 'express'
 
-// enableDebug(true)
+const app = express()
+const port = process.env.PORT || 8080
 
-const httpServer = app.listen(config.PORT, '0.0.0.0', () => {
-  console.log(`App started at port: ${config.PORT}`)
+app.get('*', (req, res) => {
+  res.send('<h1>Hello From Node on Fly!</h1>')
 })
 
-const server = new Server(httpServer)
+app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`))
 
-process.on('exit', () => {
-  console.log('Shutting down server')
-})
+// import { enableDebug } from '@example/game'
+// import { app } from './app'
+// import { config, log } from './common'
+// import { Server } from './Server'
+
+// // enableDebug(true)
+
+// const httpServer = app.listen(config.PORT, '0.0.0.0', () => {
+//   console.log(`App started at port: ${config.PORT}`)
+// })
+
+// const server = new Server(httpServer)
+
+// process.on('exit', () => {
+//   console.log('Shutting down server')
+// })
