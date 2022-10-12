21b298f8ac4ac53c9ca58768a1113c78e835bee8
Teemu Koivisto <tkoivisto456@gmail.com>
2022-10-12 21:19:35 +0300

    ci: finally fixed fly.io deployment, remove debug code

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,27 +1,16 @@
-import express from 'express'
+import { enableDebug } from '@example/game'
+import { app } from './app'
+import { config, log } from './common'
+import { Server } from './Server'
 
-const app = express()
-const port = process.env.PORT || 8080
+// enableDebug(true)
 
-app.get('*', (req, res) => {
-  res.send('<h1>Hello From Node on Fly!</h1>')
+const httpServer = app.listen(config.PORT, '0.0.0.0', () => {
+  console.log(`App started at port: ${config.PORT}`)
 })
 
-app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`))
+const server = new Server(httpServer)
 
-// import { enableDebug } from '@example/game'
-// import { app } from './app'
-// import { config, log } from './common'
-// import { Server } from './Server'
-
-// // enableDebug(true)
-
-// const httpServer = app.listen(config.PORT, '0.0.0.0', () => {
-//   console.log(`App started at port: ${config.PORT}`)
-// })
-
-// const server = new Server(httpServer)
-
-// process.on('exit', () => {
-//   console.log('Shutting down server')
-// })
+process.on('exit', () => {
+  console.log('Shutting down server')
+})
