9e946b73a0e103080b9d5380fed63a80a20e1c2d
Teemu Koivisto <tkoivisto456@gmail.com>
2022-10-19 09:35:35 +0300

    docs: update docker run command

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 docker build -t realtime-multiplayer -f ./packages/server/Dockerfile .
 
 # and to run use eg
-# docker run -p 5090:5090 realtime-multiplayer
+# docker run -it --rm -p 5090:5090 realtime-multiplayer
